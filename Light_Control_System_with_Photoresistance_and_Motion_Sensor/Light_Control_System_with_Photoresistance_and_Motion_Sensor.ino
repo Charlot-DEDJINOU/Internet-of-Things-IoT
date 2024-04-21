c430ffcbfc8d2c9b121ccf75b4dc524c09f8ae93
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-04-21 10:24:04 +0100

    comment code

--- Light_Control_System_with_Photoresistance_and_Motion_Sensor/Light_Control_System_with_Photoresistance_and_Motion_Sensor.ino
+++ Light_Control_System_with_Photoresistance_and_Motion_Sensor/Light_Control_System_with_Photoresistance_and_Motion_Sensor.ino
@@ -1,47 +1,48 @@
 #define LightPin 0
 #define PIR 2
 
-int lightReading;  // Lecture analogique de la valeur de la photorésistance avec le diviseur de tension
-int pirVal;        // Lecture du capteur de mouvement
-int minute = 0; // Compter le nombre de minutes d'inactivité dans l'endroit
-
+int lightReading;  // Variable pour stocker la lecture analogique de la photorésistance
+int pirVal;        // Variable pour stocker la lecture du capteur de mouvement
+int minute = 0;    // Variable pour compter le nombre de minutes d'inactivité
 
 void setup() {
+  pinMode(LightPin, INPUT);  // Configure la broche A0 (LightPin) comme entrée pour la photorésistance
+  pinMode(PIR, INPUT);       // Configure la broche A2 (PIR) comme entrée pour le capteur de mouvement
 
-  pinMode(LightPin, INPUT);  // Designer la broche A0 comme input
-  pinMode(PIR, INPUT);       // Designer la broche A0 comme input
-
+  // Configure les broches de 3 à 12 comme sorties pour contrôler les LEDs
   for (int ledPin = 3; ledPin < 13; ledPin++) {
-    pinMode(ledPin, OUTPUT);  // Designer la broque ledPin comme OUTPUT
+    pinMode(ledPin, OUTPUT);
   }
 
-  Serial.begin(9600);
+  Serial.begin(9600);  // Initialise la communication série à 9600 bauds
 }
 
-
 void loop() {
+  lightReading = analogRead(LightPin);  // Lit la valeur de la photorésistance
+  pirVal = digitalRead(PIR);            // Lit la valeur du capteur de mouvement
 
-  lightReading = analogRead(LightPin);  // Lecture de la valeur luminosité envoyé par la photorésistance
-  pirVal = digitalRead(PIR);            // Lecture du capteur de mouvement
-
-  if(pirVal)
-    minute = 0; // Si on detecte un mouvement on ramene la minuterie à 0
+  // Si un mouvement est détecté, réinitialise la minuterie à zéro
+  if (pirVal)
+    minute = 0;
   else {
-    delay(1000);
-    minute += 1; // Sinon on compte le nombre de seconde d'inactivité dans l'endroit
+    delay(1000);  // Attend 1 seconde pour éviter les fausses détections de mouvement
+    minute += 1;  // Incrémente la minuterie d'inactivité
   }
- 
+
+  // Si la lumière est faible et qu'il y a eu moins de 5 minutes d'inactivité
   if (lightReading < 50 && minute < 60 * 5) {
+    // Allume et éteint toutes les LEDs de 3 à 12 pour créer un effet de clignotement
     for (int ledPin = 3; ledPin < 13; ledPin++) {
-      digitalWrite(ledPin, HIGH);  // Allumer le led qui se trouve sur la broche ledPin
+      digitalWrite(ledPin, HIGH);
       delay(100);
-      digitalWrite(ledPin, LOW);  // Eteint le led qui se trouve sur la broche ledPin
+      digitalWrite(ledPin, LOW);
     }
 
+    // Allume et éteint toutes les LEDs de 12 à 3 pour créer un effet de clignotement inverse
     for (int ledPin = 12; ledPin >= 3; ledPin--) {
-      digitalWrite(ledPin, HIGH);  // Allumer le led qui se trouve sur la broche ledPin
+      digitalWrite(ledPin, HIGH);
       delay(100);
-      digitalWrite(ledPin, LOW);  // Eteint le led qui se trouve sur la broche ledPin
+      digitalWrite(ledPin, LOW);
     }
   }
 }
